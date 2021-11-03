c92c3c3d780f4784c2e40242527419dc80c26ec9
anjianshi <anjianshi@gmail.com>
2021-11-03 15:32:20 +0800

    优化 props 处理，避免 react 报警

--- dist/TransformStyleNameCreateElement.js
+++ dist/TransformStyleNameCreateElement.js
@@ -2,7 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 function TransformStyleNameCreateElement(ReactVariable, classVariables, name, rawProps, ...extra) {
     const props = { ...rawProps };
-    if (props.styleName) {
+    // 此判断同时确定了 styleName 不为空，且类型是字符串（但有可能是空字符串）
+    // styleName 是空字符串时也有必要走到 if 判断内部，因为需要删除 props 里的 styleName 属性（不然 React 会出现警告）
+    if (typeof props.styleName === 'string') {
         props.className = props.styleName
             .split(' ')
             .reduce((classNames, styleName) => ([
